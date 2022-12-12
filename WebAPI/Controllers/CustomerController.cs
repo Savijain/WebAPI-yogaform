82d4dc17aba92d079f684297082b498653ee5c5d
Savi Jain <imsavi15@gmail.com>
2022-12-12 21:13:56 +0530

    Add project files.

---
+++ WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select CustomerId, CustName, Age, Mobilenum, EmailId, Batch, PaymentDone from dbo.Registration";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CustomerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Customer cst)
+        {
+            string query = @"insert into dbo.Registration(CustomerId, CustName, Age, MobileNum, EmailId, Batch, PaymentDone) values ('" + cst.CustomerId + ", " + cst.CustName + ", " + cst.Age + ", " + cst.Mobilenum + ", " + cst.Email + ", " + cst.Batch + ", " + cst.PayStatus +@"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CustomerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+
+        public JsonResult Put(Customer cst)
+        {
+            string query = @"update dbo.Registration set Batch = '" + cst.Batch + @"' where CustomerId = " + cst.CustomerId + @" ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CustomerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        [HttpDelete("{id}")]
+
+        public JsonResult Delete(Customer cst)
+        {
+            string query = @"delete from dbo.Registration where CustomerId = " + cst.CustomerId + @" ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CustomerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
---
+++ WebAPI/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustName { get; set; }
+        public int Age { get; set; }
+        public string Mobilenum { get; set; }
+        public string Email { get; set; }
+        public string Batch { get; set; }
+        public int PayStatus { get; set; }
+    }
+}
